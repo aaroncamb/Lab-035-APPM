b62acd4b70e4a5766d1ec282d180837b30a8267a
aaroncamb <cesaar_men@hotmail.com>
2024-02-19 19:27:24 -0600

    Practica 3
    Pagina de alumnos, comunicación de componentes (padre e hijo)

---
+++ IONIC-BASIC/src/app/componentes/table-alumno/table-alumno.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-alumno',
+  templateUrl: './table-alumno.component.html',
+  styleUrls: ['./table-alumno.component.scss'],
+})
+export class TableAlumnoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
